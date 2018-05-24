6d49556a07ab1dc6b5f475bfef87293904b25160
youngjuning <1003719811@qq.com>
2018-05-24 09:54:08 +0800

    换图片

--- project/pages/navbar/index.js
+++ project/pages/navbar/index.js
@@ -1,23 +1,21 @@
-var sliderWidth = 96
-
 Page({
   data: {
-    tabs: ["陈芷尤","陈华鑫","朱千雪",], // tabs标题
+    tabs: ["钢铁侠","美国队长","蜘蛛侠","黑寡妇"], // tabs标题
     activeIndex: 0, // 默认索引
     sliderOffset: 0, // 滑块偏移量
     sliderLeft: 0, // 滑块居中偏移量，受tabs长度和sliderWidth影响
     views: [
       {
-        src: 'http://media.tantannews.com/wp-content/uploads/2017/11/329055-1.jpg'
+        src: 'http://t.cn/R1v7LR7'
       },
       {
-        src: 'http://n.sinaimg.cn/sinacn/w640h608/20180113/2344-fyqrewh6043290.jpg'
+        src: 'http://t.cn/R1v73kO'
       },
       {
-        src: 'https://astrokentico.s3.amazonaws.com/xuan/files/1b/1b7befc9-66fd-4857-af79-049eb423f6bc.jpg'
+        src: 'http://t.cn/R1vzGEk'
       },
       {
-        src: 'http://cf-xuan.digitalfive.com/getattachment/91e7ba9f-ed9d-4d3d-bd55-b0afce123f11/2-4.jpg.aspx'
+        src: 'http://t.cn/R1vz0HR'
       }
     ]
   },
@@ -25,7 +23,7 @@ Page({
     // 初始化navbar
     wx.getSystemInfo({
       success: res => {
-        let sliderWidth = res.windowWidth / this.data.tabs.length * 0.7
+        let sliderWidth = res.windowWidth / this.data.tabs.length * 0.8 // 滑动条宽度，可调整阈值
         this.setData({
           sliderLeft: (res.windowWidth / this.data.tabs.length - sliderWidth) / 2,
           sliderOffset: res.windowWidth / this.data.tabs.length * this.data.activeIndex,
